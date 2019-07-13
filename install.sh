1e56539e805f8b878ae1ff1aea817023a166ceeb
Alberto Crespo <albertocresposanchez@gmail.com>
2019-07-14 01:56:41 +0200

    Add gradle process

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_4d84b02f6973_key -iv $encrypted_4d84b02f6973_iv -in secring.gpg.enc -out ~\/.gnupg/secring.gpg -d
+openssl aes-256-cbc -K $encrypted_4d84b02f6973_key -iv $encrypted_4d84b02f6973_iv -in secring.gpg.enc -out secring.gpg -d
