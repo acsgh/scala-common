15509be34d7b9c3c91625287a039d6bc58670151
Alberto Crespo <albertocresposanchez@gmail.com>
2019-07-14 11:05:06 +0200

    Reconfigure plugin

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_4d84b02f6973_key -iv $encrypted_4d84b02f6973_iv -in secring.gpg.enc -out secring.gpg -d
+mkdir -pf /home/travis/.gnupg/
+
+openssl aes-256-cbc -K $encrypted_4d84b02f6973_key -iv $encrypted_4d84b02f6973_iv -in secring.gpg.enc -out /home/travis/.gnupg/secring.gpg -d
